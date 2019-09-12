9466548569d46895639ffd06d68afe8f52a1343a
KevinFu <814073395@qq.com>
2019-09-12 19:00:19 +0800

    编写页面布局

---
+++ DongJia/app/src/main/java/cn/ifhu/dongjia/fragments/MeFragment.java
@@ -0,0 +1,26 @@
+package cn.ifhu.dongjia.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import cn.ifhu.dongjia.R;
+import cn.ifhu.dongjia.base.BaseFragment;
+
+public class MeFragment extends BaseFragment {
+    Unbinder unbinder;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_me, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
+    }
+}
